44c76d277e399994619922c38b85e7250b82a6a9
Arthur Reimus <arthurreimus1623@gmail.com>
2022-07-26 20:07:32 +0800

    Redux using redux-toolkit

---
+++ src/old/redux-toolkit-1/bugs.js
@@ -0,0 +1,29 @@
+import { createAction, createReducer } from "@reduxjs/toolkit";
+
+// Actions
+export const bugAdded = createAction("bugAdded");
+export const bugResolved = createAction("bugResolved");
+export const bugRemoved = createAction("bugRemoved");
+
+// Reducer
+// Name of key must be same as name of action
+let lastId = 0;
+
+export default createReducer([], {
+  // Name of state can be anything
+  [bugAdded.type]: (bug, action) => {
+    bug.push({
+      id: ++lastId,
+      description: action.payload.description,
+      resolved: false,
+    });
+  },
+  [bugRemoved.type]: (bug, action) => {
+    const index = bug.findIndex((bug) => bug.id === action.payload.id);
+    bug.splice(index, 1);
+  },
+  [bugResolved.type]: (bug, action) => {
+    const index = bug.findIndex((bug) => bug.id === action.payload.id);
+    bug[index].resolved = true;
+  },
+});
